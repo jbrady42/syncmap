ae8e98b30b1f31fb893ada189a21dc91a4f232da
Jason Brady <jabrady42@gmail.com>
2016-04-20 21:25:22 -0500

    Base syncmap

---
+++ syncmap.go
@@ -0,0 +1,46 @@
+package syncmap
+
+import "sync"
+
+type Map struct {
+	data map[interface{}]interface{}
+	lock *sync.RWMutex
+}
+
+func New() *Map {
+	lock := new(sync.RWMutex)
+	tmp := make(map[interface{}]interface{})
+	return &Map{data: tmp, lock: lock}
+}
+
+func (t *Map) Set(key, value interface{}) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	t.data[key] = value
+}
+
+func (t *Map) Delete(key interface{}) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	delete(t.data, key)
+}
+
+func (t *Map) Has(key interface{}) (found bool) {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	_, found = t.data[key]
+	return found
+}
+
+func (t *Map) Get(key interface{}) (value interface{}, found bool) {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	value, found = t.data[key]
+	return value, found
+}
+
+func (t *Map) Len() int {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return len(t.data)
+}
---
+++ syncmap_test.go
@@ -0,0 +1,73 @@
+package syncmap
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	m := New()
+	if m == nil {
+		t.Error("Map is nil")
+	}
+}
+
+func TestSet(t *testing.T) {
+	m := New()
+	m.Set("test", 1)
+
+	item, ok := m.data["test"]
+
+	if !ok {
+		t.Error("Data should contain item")
+	}
+
+	if item != 1 {
+		t.Error("test should = 1")
+	}
+}
+
+func TestHas(t *testing.T) {
+	m := New()
+	m.Set("test", 1)
+
+	if !m.Has("test") {
+		t.Error("Data should contain item")
+	}
+}
+
+func TestGet(t *testing.T) {
+	m := New()
+	m.Set("test", 1)
+
+	item, ok := m.Get("test")
+
+	if !ok {
+		t.Error("Data should contain item")
+	}
+
+	if item != 1 {
+		t.Error("test should = 1")
+	}
+}
+
+func TestLen(t *testing.T) {
+	m := New()
+	m.Set("test", 1)
+	m.Set("test2", 2)
+
+	count := m.Len()
+
+	if count != 2 {
+		t.Error("Count should be 2")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	m := New()
+	m.Set("test", 1)
+	m.Set("test2", 2)
+
+	m.Delete("test2")
+
+	if m.Has("test2") {
+		t.Error("Key should have been removed")
+	}
+}
